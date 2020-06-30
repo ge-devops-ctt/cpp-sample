5167d566ffd8a6ca104f4056d48f01e9e9bdfb2f
212782121 <alexis.bel@ge.com>
2020-06-30 17:53:52 +0200

    add armv7

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ export sourcePath=src/
 export ORG_NAME=CBT
 export CHANNEL=stable
 
-rm -r ${buildPath}
+rm -r ${buildPath} || true
 conan install . --install-folder=${buildPath} --profile ${profile} -u --build:missing
 conan build . --build-folder=${buildPath}
 #conan export-pkg . ${ORG_NAME}/${CHANNEL} --source-folder=${sourcePath} --build-folder=${buildPath} --force
