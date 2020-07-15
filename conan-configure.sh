de881e3fb2ae357da349a995f9d9b3574d6f45a4
212782121 <alexis.bel@ge.com>
2020-07-15 15:18:35 +0200

    add page for code coverage

--- conan-configure.sh
+++ conan-configure.sh
@@ -4,7 +4,3 @@ conan config set proxies.no_proxy_match=https://artifactory.build.ge.com*
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan False
 conan remote update conan-center https://conan.bintray.com False
 
-conan remote add DYIYM https://artifactory.build.ge.com/api/conan/DYIYM False
-conan user -p s0ftwar3CBT -r DYIYM 502811516
-conan remote add DYIYM-SNAPSHOT https://artifactory.build.ge.com/api/conan/DYIYM-SNAPSHOT False
-conan user -p s0ftwar3CBT -r DYIYM-SNAPSHOT 502811516
